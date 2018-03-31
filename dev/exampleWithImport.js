d4f891f4fbe9e50b023680c904b463f684f54aa0
Dennis Teo <dennis@anel.co>
2018-03-31 18:20:03 +0200

    dev clean up

--- dev/exampleWithImport.js
+++ dev/exampleWithImport.js
@@ -1,14 +1,14 @@
-import {envGlobal, isBrowser, isNode} from '../src/index';
+import {dynaUniversal, isBrowser, isNode} from '../src/index';
 
 // we import the `env`
-envGlobal;            // is the node.js's global or browser's window
-envGlobal.isNode;     // boolean if we are in node
-envGlobal.isBrowser;  // boolean is we are is browser
+dynaUniversal;            // is the node.js's global or browser's window
+dynaUniversal.isNode;     // boolean if we are in node
+dynaUniversal.isBrowser;  // boolean is we are is browser
 
 // console them
-console.log('Are we are under node? Answer: ', envGlobal.isNode);       // writes, true or false
-console.log('Are we are under browser? Answer: ', envGlobal.isBrowser); // writes, true or false
+console.log('Are we are under node? Answer: ', dynaUniversal.isNode);       // writes, true or false
+console.log('Are we are under browser? Answer: ', dynaUniversal.isBrowser); // writes, true or false
 
 // we can import the functions also from it
-console.log('Are we are under node? Answer: ', isNode());       // writes, true or false
-console.log('Are we are under browser? Answer: ', isBrowser()); // writes, true or false
+console.log('Are we are under node? Answer: ', isNode);       // writes, true or false
+console.log('Are we are under browser? Answer: ', isBrowser); // writes, true or false
--- dev/index.ts
+++ dev/index.ts
@@ -1,10 +1,2 @@
-declare let window: any;
-declare let global: any, describe: any, clearTest: any, it: any, expect: any;
-import "./mock-jest";
-
-import './debug-typescript';  // debug typescript code
-
 import './exampleWithGlobals.js';  // debug typescript code
 import './exampleWithImport.js';  // debug typescript code
-
-// import '../tests/mix-use.test'; // debug any test (experimental)
